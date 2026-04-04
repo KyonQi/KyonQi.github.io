ff3208fc9f379a19f7e348538e3f251d9808be3c
KyonQi <kyon942755317@hotmail.com>
2026-04-04 15:40:23 +0200

    Site updated: YYYY-04-Apr 4, 2026 15:40:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1775231097664";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1775310022081";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
